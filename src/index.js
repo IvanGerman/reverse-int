74960353d56dde937f946159786d4974b92d30e3
IvanGerman <ivgvi@mail.ru>
2021-08-20 21:30:07 +0700

    feat: implemented function reverse(n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = n.toString();
+  let arr = [];
+  for ( let i = 0; i < n.length; i++ ) {
+    arr.unshift(n[i]);
+  };
+  let result = arr.join('');
+  return parseInt(result);
 }
